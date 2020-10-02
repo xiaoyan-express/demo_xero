782a8718dbafbc51a5eff5fdd5b76bb84a564ca5
xiaoyan z <xyzxiaoyan@gmail.com>
2020-10-02 12:38:54 -0700

    Create the eclipse project - initial version

---
+++ src/main/java/com/ta/demo_xero/App.java
@@ -0,0 +1,13 @@
+package com.ta.demo_xero;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
